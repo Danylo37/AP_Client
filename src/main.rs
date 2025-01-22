5d57e5437476fd06a9c6dbbda3659da214776789
Danylo Lopatin <d.s.lopatin@student.khai.edu>
2025-01-22 13:49:26 +0100

    -

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use crate::network_initializer::NetworkInit;
-
 mod general_use;
 mod clients;
 mod network_initializer;
 mod ui;
 mod simulation_controller;
 mod servers;
+use clients::client_danylo::test_gui::test_gui;
 
 fn main() {
     env_logger::init();
